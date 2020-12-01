7d40ddcd85a0c95cf93c99b122b7b92a4f1f59cd
Marco Schlicht <marcoschlicht@onlinehome.de>
2020-12-01 19:26:04 +0100

    solved part 2 in 01_3.py

--- 01_3.py
+++ 01_3.py
@@ -23,11 +23,29 @@ def find_nums(numbers: List[int], number_sum: int) -> Tuple[int, int]:
     raise Exception("Sum is not possible")
 
 
+def find_nums_part_2(numbers: List[int], number_sum: int) -> Tuple[int, int, int]:
+    """Finds three numbers in the list that add up to 2020.
+
+    Complexity: 2n log n
+    """
+    for pos1, num1 in enumerate(numbers):
+        for pos2, num2 in enumerate(numbers):
+            sub = number_sum - num1 - num2
+            # start searching at next position
+            pos3 = bisect.bisect_left(numbers, sub, pos1 + 1)
+            if pos3 != len(numbers) and numbers[pos3] == sub:
+                return (num1, numbers[pos3], num2)
+    raise Exception("Sum is not possible")
+
+
 def main():
     numbers: List[int] = read_nums("input.txt")
     num1, num2 = find_nums(numbers, 2020)
     print(num1, num2)
     print(num1 * num2)
+    num1, num2, num3 = find_nums_part_2(numbers, 2020)
+    print(num1, num2, num3)
+    print(num1 * num2 * num3)
 
 
 if __name__ == '__main__':
