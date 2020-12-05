df6771f17b1b161a70b067fcd5f41ca3d1b5e665
Marco Schlicht <marcoschlicht@onlinehome.de>
2020-12-05 16:11:13 +0100

    added comment

--- 03/vim/03_part1.sh
+++ 03/vim/03_part1.sh
@@ -8,7 +8,7 @@ echo "Traversal: $STEP_DOWN - $STEP_RIGHT"
 vim -u vimrc\
     --cmd "let step_down = $STEP_DOWN"\
     --cmd "let step_right = $STEP_RIGHT"\
-    +"normal @q"\
+    +"normal @q" `# execute q macro`\
     +"normal ggdG" `# delete pattern`\
     +"put =tree_counter" `# insert count`\
     +"normal kdd" `# delete emptyline`\
