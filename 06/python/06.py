aa64c57f15d236aa35c27fafee5f2dddee46c29c
Marco Schlicht <marcoschlicht@onlinehome.de>
2020-12-07 07:20:51 +0100

    fixed typing in 06

--- 06/python/06.py
+++ 06/python/06.py
@@ -2,13 +2,13 @@ from typing import List
 from functools import reduce
 
 
-def part1(filename: str) -> List[int]:
+def part1(filename: str) -> int:
     with open(filename) as file:
         data = map(lambda block: len(set(block.replace("\n", ""))), file.read().split("\n\n"))
         return sum(data)
 
 
-def part2(filename: str) -> List[int]:
+def part2(filename: str) -> int:
     with open(filename) as file:
         return sum(map(lambda x: len(reduce(lambda fs1, fs2: fs1.intersection(fs2), x)), map(lambda block: map(set, filter(None, block.split("\n"))), file.read().split("\n\n"))))
 
